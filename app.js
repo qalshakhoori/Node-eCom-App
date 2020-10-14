580d96569c3029086727c3377ef0262f503653a1
Qassim Alshakhoori <qalshakhoori@moic.gov.bh>
2020-10-14 10:00:07 +0300

    cart

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const shopRoutes = require('./routes/shop');
 
 const errorsController = require('./controllers/errors');
 const mongoConnect = require('./util/database').mongoConnect;
+const User = require('./models/user');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -20,13 +21,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 // This block is only needed because we don't have
 // an authentication mechanisme yet
 app.use((req, res, next) => {
-  // User.findByPk(1)
-  //   .then((user) => {
-  //     req.user = user;
-  //     next();
-  //   })
-  //   .catch((err) => console.log(err));
-  next();
+  User.findById('5f868fff4bf5ab00074e9579')
+    .then((user) => {
+      req.user = new User(user.name, user.email, user.cart, user._id);
+      next();
+    })
+    .catch((err) => console.log(err));
 });
 ///////////////////////////////////////////////////
 
@@ -36,6 +36,5 @@ app.use(shopRoutes);
 // app.use(errorsController.get404);
 
 mongoConnect((client) => {
-  console.log(client);
   app.listen(3001);
 });
--- controllers/admin.js
+++ controllers/admin.js
@@ -13,12 +13,17 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(title, price, description, imageUrl);
-  // Product.create({})
+  const product = new Product(
+    title,
+    price,
+    description,
+    imageUrl,
+    null,
+    req.user._id
+  );
   product
     .save()
     .then((result) => {
-      console.log(result);
       res.redirect('/admin/products');
     })
     .catch((err) => console.log(err));
@@ -72,19 +77,17 @@ exports.getProducts = (req, res, next) => {
     .catch((err) => console.log(err));
 };
 
-// exports.postDeleteProduct = (req, res, next) => {
-//   const prodId = req.body.productId;
-//   Product.findByPk(prodId)
-//     .then((product) => {
-//       return product.destroy();
-//     })
-//     .then((result) => {
-//       console.log('DESTROY PRODUCT');
-//       res.redirect('/admin/products');
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//     });
+exports.postDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.deleteById(prodId)
+    .then((result) => {
+      console.log('DESTROY PRODUCT');
+      res.redirect('/admin/products');
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
 
 //   res.redirect('/');
 // };
