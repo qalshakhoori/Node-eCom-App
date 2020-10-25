56e8974546ffa2e5622c0e1ad53b9b48cc6dd5c5
Qassim Alshakhoori <qassim.alshakhoori@gmail.com>
2020-10-25 09:51:35 +0300

    storing session in mongodb

--- app.js
+++ app.js
@@ -3,8 +3,16 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoDbStore = require('connect-mongodb-session')(session);
+
+const MONGODB_URI = 'mongodb://nodejs:Password1234@127.0.0.1:27017/shop';
 
 const app = express();
+const store = new MongoDbStore({
+  uri: MONGODB_URI,
+  collection: 'sessions',
+});
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -16,10 +24,17 @@ const authRoutes = require('./routes/auth');
 const errorsController = require('./controllers/errors');
 // const mongoConnect = require('./util/database').mongoConnect;
 const User = require('./models/user');
-const { use } = require('./routes/admin');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  session({
+    secret: 'A very long complex secret string',
+    resave: false,
+    saveUninitialized: false,
+    store: store,
+  })
+);
 
 // This block is only needed because we don't have
 // an authentication mechanisme yet
@@ -40,7 +55,7 @@ app.use(authRoutes);
 app.use(errorsController.get404);
 
 mongoose
-  .connect('mongodb://nodejs:Password1234@127.0.0.1:27017/shop')
+  .connect(MONGODB_URI)
   .then(() => {
     User.findOne().then((user) => {
       if (!user) {
@@ -54,6 +69,7 @@ mongoose
         user.save();
       }
     });
+    console.log('listening on port 3001');
     app.listen(3001);
   })
   .catch(() => {
