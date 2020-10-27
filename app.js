9c0e7a98d7a851107a0558cc41b1a58fc415af63
Qassim Alshakhoori <qassim.alshakhoori@gmail.com>
2020-10-27 10:07:21 +0300

    signup

--- app.js
+++ app.js
@@ -54,21 +54,9 @@ app.use(authRoutes);
 app.use(errorController.get404);
 
 mongoose
-  .connect(MONGODB_URI)
-  .then((result) => {
-    User.findOne().then((user) => {
-      if (!user) {
-        const user = new User({
-          name: 'Max',
-          email: 'max@test.com',
-          cart: {
-            items: [],
-          },
-        });
-        user.save();
-      }
-    });
-    app.listen(3000);
+  .connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    app.listen(3001);
   })
   .catch((err) => {
     console.log(err);
